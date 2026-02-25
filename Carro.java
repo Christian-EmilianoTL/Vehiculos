ae77d590a936657a3e9258851c6b498f078fb0c8
Christian-EmilianoTL <20253rd026@utez.edu.mx>
2026-02-25 17:53:59 -0600

    Definiendo variables

--- Carro.java
+++ Carro.java
@@ -1,3 +1,16 @@
 public class Carro{
+    protected String marca;
+    protected String modelo;
+    protected String velocidadMaxima;
+
+
+    public Carro(String marca,String modelo,String velocidadMaxima){
+
+        this.marca=marca;
+        this.modelo=modelo;
+        this.velocidadMaxima=velocidadMaxima;
+
+    }
+
     
 }
